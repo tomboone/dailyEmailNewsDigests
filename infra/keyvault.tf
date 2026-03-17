a16f596e0ff8b8edf316bf41987b266356c47348
Tom Boone <tom.boone@ingest.ai>
2026-03-17 15:59:32 -0400

    add SP policy for KV in Open Tofu

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_key_vault_access_policy" "terraform" {
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  object_id    = var.terraform_sp_object_id
 
   secret_permissions = ["Get", "Set", "Delete", "List"]
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -17,3 +17,8 @@ variable "app_service_plan_resource_group" {
   description = "Resource group containing the existing App Service Plan"
   type        = string
 }
+
+variable "terraform_sp_object_id" {
+  description = "Object ID of the service principal used to run Terraform (for Key Vault access policy)"
+  type        = string
+}
